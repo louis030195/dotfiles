d0b19f1a3b5638579a577418f2204337f5e05dc9
louis <louis.beaumont@gmail.com>
2020-09-12 17:16:15 +0200

    Obsidian, drawio

--- bin/setup_new_env.sh
+++ bin/setup_new_env.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+
+apt install -y wget curl tmux
+# Rust / Cargo
+curl https://sh.rustup.rs -sSf | sh
+# Alacritty
+cargo install alacritty
+
+mkdir -p $HOME/binaries
 cp .tmux.conf .gitignore $HOME
+wget https://github.com/jgraph/drawio-desktop/releases/download/v13.6.2/draw.io-x86_64-13.6.2.AppImage -O $HOME/binaries
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/v0.8.12/Obsidian-0.8.12.AppImage -O $HOME/binaries
