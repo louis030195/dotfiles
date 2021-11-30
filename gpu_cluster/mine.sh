04bbba81a5a54271af7d622262c2c49f00c1d716
louis030195 <louis.beaumont@gmail.com>
2021-11-30 10:37:02 +0100

    stuff

--- gpu_cluster/mine.sh
+++ gpu_cluster/mine.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# ETH
+#ADDR="0x6cA39bC556132d3880651A3e6c9a5858a615E64D"
+# BTC
+# BINANCE
+#ADDR="17YnysY1Vz99VgkQoUdmjDruUtx1ogpFyX"
+# COINBASE
+ADDR="3BF8sF7wMPtjKZPnhdCfDswKgWNt3sJrH2"
+./t-rex -a ethash -o stratum+tcp://eth.2miners.com:2020 -u "$ADDR"  --lhr-autotune-mode full --api-bind-http "0.0.0.0:4067"
+
+
 # 62 mhs 0.7% fee
-./1.35/mine_eth.sh
+#./1.35/mine_eth.sh
 
 # 64 mhs 0.8% fee
 #./miner --algo ethash --server eth.2miners.com:2020 --user 0x6cA39bC556132d3880651A3e6c9a5858a615E64D
