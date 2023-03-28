70088b8c84a5be63f92467091a422a5c288951de
Louis Beaumont <louis.beaumont@gmail.com>
2023-03-28 16:18:08 +0200

    yoo

--- bin/git.sh
+++ bin/git.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cp globalgitignore ~/.gitignore
 git config user.name "louis030195"
 git config user.email "louis.beaumont@gmail.com"
 git config --global core.excludesfile ~/.gitignore
